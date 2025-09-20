ea9904ed36b36579c4a33fd8a3205a25f0a3a63c
Connor Fitzsimmons <connorkfitzsimmons@gmail.com>
2025-09-19 22:00:37 -0400

    Initial Commit

---
+++ south-west-exposures-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  south-west-exposures-ios
+//
+//  Created by Connor Fitzsimmons on 2025-09-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ south-west-exposures-ios/south_west_exposures_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  south_west_exposures_iosApp.swift
+//  south-west-exposures-ios
+//
+//  Created by Connor Fitzsimmons on 2025-09-19.
+//
+
+import SwiftUI
+
+@main
+struct south_west_exposures_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
